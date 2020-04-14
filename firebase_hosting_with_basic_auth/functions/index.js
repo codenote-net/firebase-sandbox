f7e86fc1293a8cd24ee56e4601ecdf9ad6fa105c
Tadashi Shigeoka <shigeoka@codenote.net>
2020-04-14 23:01:52 +0900

    Apply Basic Auth using express + basic-auth-connect
    - [FirebaseでBasic認証をかけてサイトを作る\(Hostingする\) \- Qiita](https://qiita.com/567000/items/65f55eda8d7c6df09138)

--- firebase_hosting_with_basic_auth/functions/index.js
+++ firebase_hosting_with_basic_auth/functions/index.js
@@ -1,8 +1,12 @@
-const functions = require('firebase-functions');
+const functions = require('firebase-functions')
+const express = require('express')
+const basicAuth = require('basic-auth-connect')
+const app = express()
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+app.all('/*', basicAuth((user, password) => {
+  return user === 'youruser' && password === 'yourpassword';
+}));
+
+app.use(express.static(`${__dirname}/static/`))
+
+exports.app = functions.https.onRequest(app)
